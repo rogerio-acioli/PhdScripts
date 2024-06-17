712f30b753dc421fa5571c9b24bd6befd4875486
origin <rogerio.acioli@hotmail.com.br>
2024-06-16 21:25:43 -0300

    Version 000 - Commit and Push na Branch correta

--- conwayslaw.py
+++ conwayslaw.py
@@ -1,15 +1,12 @@
 #Calcula a Complexidade e a previsão de qualidade de um produto desenvolvido de acordo com o modelo de estrutura organizacional
-
 import pandas as pd
 
-
 # Função para calcular o nível de complexidade organizacional
-def calcular_complexidade(NOE, OS, CC):
+def calcular_complexidade(membros, OS, CC):
     # Aqui, definimos a complexidade como uma combinação linear simples das métricas
-    # Você pode ajustar os pesos conforme necessário
-    complexidade = 0.3 * NOE + 0.4 * OS + 0.3 * CC
+    # Ajuste os pesos conforme necessário
+    complexidade = 0.3 * membros + 0.4 * OS + 0.3 * CC
     return complexidade
-
 
 # Função para prever a qualidade do produto
 def prever_qualidade(complexidade):
@@ -19,29 +16,38 @@ def prever_qualidade(complexidade):
     qualidade = 100 / (1 + complexidade)
     return qualidade
 
-
 # Leitura do arquivo de entrada
-input_file = 'C:/Users/ra52440/OneDrive - AGCO Corp/00. ROGERIO ALVES_Documentos/Python/dados_entrada.xlsx'
+input_file = 'C:/Users/roger/OneDrive/Doutorado/Python/dados_entrada.xlsx'
 df = pd.read_excel(input_file)
 
-# Inicialização das colunas de saída
-df['Complexidade Organizacional'] = 0
-df['Qualidade Prevista'] = 0
+# Verificação da validade dos dados
+for index, row in df.iterrows():
+    membros = row['Membros']
+    OS = row['Org_Size']
+
+    if OS < membros:
+        raise ValueError(
+            f"A linha {index + 1} tem um valor de Org_Size ({OS}) menor que o número de Membros ({membros}).")
+
+# Inicialização das colunas de saída com tipo float
+df['Complexidade Organizacional'] = 0.0
+df['Qualidade Prevista'] = 0.0
 
 # Cálculo da complexidade e qualidade para cada linha do dataframe
 for index, row in df.iterrows():
-    NOE = row['NOE']
+    membros = row['Membros']
     OS = row['Org_Size']
     CC = row['CC']
 
-    complexidade = calcular_complexidade(NOE, OS, CC)
+    complexidade = calcular_complexidade(membros, OS, CC)
     qualidade = prever_qualidade(complexidade)
 
     df.at[index, 'Complexidade Organizacional'] = complexidade
     df.at[index, 'Qualidade Prevista'] = qualidade
 
 # Escrita dos resultados no arquivo de saída
-output_file = 'C:/Users/ra52440/OneDrive - AGCO Corp/00. ROGERIO ALVES_Documentos/Python/resultado_qualidade.xlsx'
+output_file = 'C:/Users/roger/OneDrive/Doutorado/Python/resultado_qualidade.xlsx'
 df.to_excel(output_file, index=False)
 
 print(f'Resultados salvos em {output_file}')
+
